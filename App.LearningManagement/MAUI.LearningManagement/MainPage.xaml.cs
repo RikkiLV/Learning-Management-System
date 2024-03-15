7a6fb76b00bf9d2b7c72f56f43380574c072eb7b
Rikki Lee <rsv21@fsu.edu>
2024-03-15 17:07:51 -0400

    Adding functionality for student and instructor view. Closes #31

--- App.LearningManagement/MAUI.LearningManagement/MainPage.xaml.cs
+++ App.LearningManagement/MAUI.LearningManagement/MainPage.xaml.cs
@@ -13,16 +13,13 @@ namespace MAUI.LearningManagement
 
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void StudentClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Shell.Current.GoToAsync("//Student");
+        }
+        private void InstructorClicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync("//Instructor");
         }
     }
 
