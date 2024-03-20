287a47d08c26e1c2028d3a648419ceb039a53e1b
Rikki Lee <rsv21@fsu.edu>
2024-03-20 10:28:03 -0400

    Fixing bug of previous student data being displayed. Closes #45

--- App.LearningManagement/MAUI.LearningManagement/ViewModels/PersonDetailViewModel.cs
+++ App.LearningManagement/MAUI.LearningManagement/ViewModels/PersonDetailViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Content;
+using Library.LearningManagement.Models;
+using Library.LearningManagement.Services;
 
 namespace MAUI.LearningManagement.ViewModels
 {
@@ -11,5 +14,27 @@ namespace MAUI.LearningManagement.ViewModels
 
         public string Name { get; set; }
         public string ClassificationString { get; set; } 
+
+        public void AddPerson()
+        {
+            var person = new Person();
+            PersonClassification classification;
+            switch (ClassificationString)
+            {
+                case "S":
+                    classification = PersonClassification.Senior; break;
+                case "J":
+                    classification = PersonClassification.Junior; break;
+                case "O":
+                    classification = PersonClassification.Sophomore; break;
+                case "F":
+                default:
+                    // Handle unexpected values
+                    classification = PersonClassification.Freshman;
+                    break;
+            }
+            StudentService.Current.Add(new Student { Name = Name, Classification = classification });
+            Shell.Current.GoToAsync("//Instructor");
+        }
     }
 }
--- App.LearningManagement/MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
+++ App.LearningManagement/MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
@@ -14,24 +14,17 @@ public partial class PersonDetailView : ContentPage
 
 	private void OkClick(object sender, EventArgs e)
 	{
-		var context = BindingContext as PersonDetailViewModel;
-		PersonClassification classification;
-		switch (context.ClassificationString)
-		{
-			case "S":
-				classification = PersonClassification.Senior; break;
-            case "J":
-                classification = PersonClassification.Junior; break;
-            case "O":
-                classification = PersonClassification.Sophomore; break;
-            case "F":
-                classification = PersonClassification.Freshman; break;
-            default:
-                // Handle unexpected values
-                classification = PersonClassification.Freshman;
-                break;
-        }
-		StudentService.Current.Add(new Student { Name = context.Name, Classification = classification });
-		Shell.Current.GoToAsync("//MainPage");
+		//To do: Migrate this to ViewModel
+		(BindingContext as PersonDetailViewModel).AddPerson();
 	}
+
+	private void OnLeaving (object sender, NavigatedToEventArgs e)
+	{
+		BindingContext = null;
+	}
+
+    private void OnArriving(object sender, NavigatedFromEventArgs e)
+    {
+		BindingContext = new PersonDetailViewModel();
+    }
 }
